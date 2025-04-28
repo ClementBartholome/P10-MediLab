afaeb6883b8aac55a8b00e63e34ef6574af57b24
Clément Bartholomé <clementoss@gmail.com>
2025-04-28 16:52:51 +0200

    initial commit

---
+++ PatientsAPI/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿using P10___MédiLabo___Patients_API.Enums;
+
+namespace P10___MédiLabo___Patients_API.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string Prenom { get; set; }
+    public string Nom { get; set; }
+    public DateTime DateNaissance { get; set; }
+    public GenreEnum Genre { get; set; }
+    public string? Adresse { get; set; }
+    public string? Telephone { get; set; }
+}
