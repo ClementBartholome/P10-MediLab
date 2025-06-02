c80a73fbb2b7c5a548147fc7007c9f124820a1f3
Clément Bartholomé <clementoss@gmail.com>
2025-06-02 10:02:15 +0200

    feat(patients): display name pour la date de naissance

--- Frontend/Models/PatientViewModel.cs
+++ Frontend/Models/PatientViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace P10___MédiLabo_Solutions.Models;
+﻿using System.ComponentModel;
+
+namespace P10___MédiLabo_Solutions.Models;
 
 public class PatientViewModel
 {
     public int Id { get; set; }
     public string Prenom { get; set; }
     public string Nom { get; set; }
+    [DisplayName ("Date de naissance")]
     public DateTime DateNaissance { get; set; }
     public int Genre { get; set; }
     public string Adresse { get; set; }
