13adc513e03f89a7d79fd6fb3aaf77d40a3dfad3
Clément Bartholomé <clementoss@gmail.com>
2025-07-04 11:57:24 +0200

    feat(patients): migration seed data

--- PatientsAPI/Migrations/20250704092752_SeedTablePatients.Designer.cs
+++ PatientsAPI/Migrations/20250704092752_SeedTablePatients.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using P10___MédiLabo___Patients_API.Data;
 
@@ -11,9 +12,11 @@ using P10___MédiLabo___Patients_API.Data;
 namespace P10___MédiLabo___Patients_API.Migrations
 {
     [DbContext(typeof(PatientsDbContext))]
-    partial class PatientsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250704092752_SeedTablePatients")]
+    partial class SeedTablePatients
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PatientsAPI/Migrations/20250704092752_SeedTablePatients.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace P10___MédiLabo___Patients_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTablePatients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "Id", "Adresse", "DateNaissance", "Genre", "Nom", "Prenom", "Telephone" },
+                values: new object[,]
+                {
+                    { 1, "1 Brookside St", new DateTime(1966, 12, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "TestNone", "Test", "100-222-3333" },
+                    { 2, "2 High St", new DateTime(1945, 6, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "TestBorderline", "Test", "200-333-4444" },
+                    { 3, "3 Club Road", new DateTime(2004, 6, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "TestInDanger", "Test", "300-444-5555" },
+                    { 4, "4 Valley Dr", new DateTime(2002, 6, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "TestEarlyOnset", "Test", "400-555-6666" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
