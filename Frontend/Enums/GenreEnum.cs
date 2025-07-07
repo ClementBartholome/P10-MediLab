47ce4ad7c2573e67cbfdd5be53f58c8f0b6a703e
Clément Bartholomé <clementoss@gmail.com>
2025-07-07 16:56:21 +0200

    refactor(frontend): suppression de la référence vers le projet PatientsAPI

---
+++ Frontend/Enums/GenreEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P10___MédiLabo_Solutions.Enums;
+
+public enum GenreEnum
+{
+    [Display(Name = "Masculin")]
+    M = 1,
+    [Display(Name = "Féminin")]
+    F = 2,
+    [Display(Name = "Autre")]
+    Autre = 3
+}
---
+++ Frontend/Extensions/EnumExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P10___MédiLabo_Solutions.Extensions;
+
+public static class EnumExtension
+{
+    public static string GetDisplayName(this Enum enumValue)
+    {
+        var displayAttribute = enumValue.GetType()
+            .GetField(enumValue.ToString())
+            ?.GetCustomAttributes(typeof(DisplayAttribute), false)
+            .FirstOrDefault() as DisplayAttribute;
+
+        return displayAttribute?.Name ?? enumValue.ToString();
+    }
+}
